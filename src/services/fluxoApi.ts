61d748e1accaddd4354fb88b3f11ab7fcac3d5a7
GabrielAthenaOffice <gabrielathenaoffice@gmail.com>
2025-12-05 17:55:38 -0300

    ultima atualizacao. terça deve funcionar

--- src/services/fluxoApi.ts
+++ src/services/fluxoApi.ts
@@ -6,6 +6,7 @@ const FLUXOS_API_URL = process.env.REACT_APP_FLUXOS_API_URL || 'https://fluxosda
 
 const fluxoApi = axios.create({
     baseURL: FLUXOS_API_URL,
+    withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
     },
